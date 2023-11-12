60f2745ae2bb0f90aa747797989b59a5bbe2a566
Alexandr SARANDI <sarandi.alex@gmail.com>
2023-11-12 02:06:13 -0800

    added register defines

--- nesmu.h
+++ nesmu.h
@@ -4,6 +4,37 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#define PPUCTRL 0x2000
+#define PPUMASK 0x2001
+#define PPUSTATUS 0x2002
+#define OAMADDR 0x2003
+#define OAMDATA 0x2004
+#define PPUSCROLL 0x2005
+#define PPUADDR 0x2006
+#define PPUDATA 0x2007
+#define SQ1_VOL 0x4000
+#define SQ1_SWEEP 0x4001
+#define SQ1_LO 0x4002
+#define SQ1_HI 0x4003
+#define SQ2_VOL 0x4004
+#define SQ2_SWEEP 0x4005
+#define SQ2_LO 0x4006
+#define SQ2_HI 0x4007
+#define TRI_LINEAR 0x4008
+#define TRI_LO 0x400A
+#define TRI_HI 0x400B
+#define NOISE_VOL 0x400C
+#define NOISE_LO 0x400E
+#define NOISE_HI 0x400F
+#define DMC_FREQ 0x4010
+#define DMC_RAW 0x4011
+#define DMC_START 0x4012
+#define DMC_LEN 0x4013
+#define OAMDMA 0x4014
+#define SND_CHN 0x4015
+#define JOY1 0x4016
+#define JOY2 0x4017
+
 typedef struct cpu {
     uint8_t A, X, Y, S, P, u8, last_read;
     uint16_t PC, u16;
--- ppu.c
+++ ppu.c
@@ -3,16 +3,6 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define PPUCTRL 0x2000
-#define PPUMASK 0x2001
-#define PPUSTATUS 0x2002
-#define OAMADDR 0x2003
-#define OAMDATA 0x2004
-#define PPUSCROLL 0x2005
-#define PPUADDR 0x2006
-#define PPUDATA 0x2007
-#define OAMDMA 0x4014
-
 uint8_t ppu_read(t_nes *nes, uint16_t addr) {
     switch (addr) {
     case PPUSTATUS:
